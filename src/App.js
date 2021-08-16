b71bcf2f6e20811b28867aa8d5cffea4f30550e9
Abhay Nimavat <abhaynimavat2410@gmail.com>
2021-08-16 10:01:02 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+ import React from 'react';
+import UseEffect from './component/hooks/useEffect';
+import UseReducer from './component/hooks/useReducer';
+ import UseState from "./component/hooks/useState";
+ 
+ const App = () => {
+   return <UseEffect />
+ }
+ 
+ export default App
+ 
